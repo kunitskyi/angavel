df242a20bf7289f1b1617c0045715e67a03935fa
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-04-14 16:50:48 -0400

    add: project stub function

---
+++ lib/logic/project.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+##
+#
+# Project Logic
+#
+#######
+
+function angavel:project:start-fresh {
+    
+}
+
+function angavel:project:start-update {
+    
+}
+
+function angavel:project:start {
+    
+}
+
+function angavel:project:stop {
+    
+}
