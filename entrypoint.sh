7dc9c019a4baaedf449058d4ea309738ea51b0d6
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-04-14 16:43:45 -0400

    edit: make entrypoint follow guidelines

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,13 +8,13 @@
 
 GLOBAL_MODULE_PWD="${GLOBAL_PWD}/module/angavel"
 
-source "${GLOBAL_MODULE_PWD}/config/selectors.sh"
+source "${GLOBAL_MODULE_PWD}/.env/${GLOBAL_CURRENT_ENVIRONMENT}/selectors.sh"
 source "${GLOBAL_MODULE_PWD}/lib/.index.sh"
 
-function module-entrypoint { # FUNCTION NAME is standard
+function ws:module-entrypoint { # FUNCTION NAME is standard
     
-    selector-logic GLOBAL_MODULE_MAIN_SELECTOR GLOBAL_MODULE_MAIN_DESCRIPTION
+    angavel:selector:main
     
 }
 
-module-entrypoint
+ws:module-entrypoint
