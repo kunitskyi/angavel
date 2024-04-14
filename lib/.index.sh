0ae02de816b0091fef5afde4eb5fc2d5ed9360ee
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-04-14 16:47:55 -0400

    add: basic indexes

---
+++ lib/.index.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+##
+#
+# MODULE LIBRARY Index File
+#
+#######
+
+function MODULE-LIBRARY-INDEX {
+    
+    local LOCAL_PWD="${GLOBAL_MODULE_PWD}/lib"
+    
+    source $LOCAL_PWD/logic/.index.sh
+    source $LOCAL_PWD/selector/.index.sh
+    
+}
+
+MODULE-LIBRARY-INDEX
---
+++ lib/logic/.index.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+##
+#
+# MODULE LIBRARY LOGIC Index File
+#
+#######
+
+function MODULE-LIBRARY-LOGIC-INDEX {
+    
+    local LOCAL_PWD="${GLOBAL_MODULE_PWD}/lib/logic"
+    
+    source $LOCAL_PWD/project.sh
+    
+}
+
+MODULE-LIBRARY-LOGIC-INDEX
