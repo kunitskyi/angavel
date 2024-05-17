9d4c9321dda6dca365180583463e537e886e2773
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-05-17 13:03:19 -0400

    add: module for auto generating env variables

---
+++ lib/logic/initialization/argument.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+##
+#
+# Module Argument Initialization
+#
+#######
+
+function angavel:initialization:argument {
+    
+    local DOCKER_ENV_PATH="${GLOBAL_MODULE_PWD}/.env/${GLOBAL_CURRENT_ENVIRONMENT}/docker.env"
+    
+    @edit-env-file \
+    "${DOCKER_ENV_PATH}" \
+    "AG_MODULE_PATH" \
+    "${GLOBAL_MODULE_PWD}/"
+    
+    @edit-env-file \
+    "${DOCKER_ENV_PATH}" \
+    "AG_NETWORK_NAME" \
+    "${GLOBAL_MODULE_NETWORK_NAME}"
+}
