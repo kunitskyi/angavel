20b9779ae96ca960e2f32afe246f33a34493e843
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-05-17 12:58:09 -0400

    edit: library logic index

--- lib/logic/.index.sh
+++ lib/logic/.index.sh
@@ -10,7 +10,9 @@ function MODULE-LIBRARY-LOGIC-INDEX {
     
     local LOCAL_PWD="${GLOBAL_MODULE_PWD}/lib/logic"
     
+    source $LOCAL_PWD/initialization/.index.sh
     source $LOCAL_PWD/project.sh
+    source $LOCAL_PWD/project/.index.sh
     
 }
 
