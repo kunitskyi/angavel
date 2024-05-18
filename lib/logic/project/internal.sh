239becb1eb5ed2bba12bda43c46370ba1474476d
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-05-18 09:43:52 -0400

    edit: spread project init in separate functions

--- lib/logic/project/internal.sh
+++ lib/logic/project/internal.sh
@@ -29,3 +29,69 @@ function angavel:project:_stop {
     "down"
     
 }
+
+function  angavel:project:init {
+    
+    angavel:project:init:_srp
+    angavel:project:init:_spa
+    angavel:project:init:_api
+    
+}
+
+function  angavel:project:init:_srp {
+    
+}
+
+function  angavel:project:init:_api {
+    
+    local COMPOSE_PATH="${GLOBAL_COMPOSE_PWD}/API/docker.yml"
+    
+    git clone \
+    -b "${ENV_API_GIT_BRANCH}" \
+    "${ENV_API_GIT_URL}" \
+    "${GLOBAL_MODULE_PWD}/data/${GLOBAL_CURRENT_ENVIRONMENT}/API"
+    
+    angavel:project:_start "API"
+    ws:docker:compose:send-cmd \
+    "${GLOBAL_COMPOSE_ENV_PATH}" "${COMPOSE_PATH}" \
+    "api_cgi" "chmod +x /cgi-cmd.sh"
+    ws:docker:compose:send-cmd \
+    "${GLOBAL_COMPOSE_ENV_PATH}" "${COMPOSE_PATH}" \
+    "api_cgi" "/cgi-cmd.sh change-group/owner"
+    ws:docker:compose:send-cmd \
+    "${GLOBAL_COMPOSE_ENV_PATH}" "${COMPOSE_PATH}" \
+    "api_cgi" "/cgi-cmd.sh composer-install"
+    ws:docker:compose:send-cmd \
+    "${GLOBAL_COMPOSE_ENV_PATH}" "${COMPOSE_PATH}" \
+    "api_cgi" "/cgi-cmd.sh artisan-key-gen"
+    angavel:project:_stop "API"
+    
+}
+
+function  angavel:project:init:_spa {
+    
+    local COMPOSE_PATH="${GLOBAL_COMPOSE_PWD}/SPA/docker.yml"
+    
+    git clone \
+    -b "${ENV_SPA_GIT_BRANCH}" \
+    "${ENV_SPA_GIT_URL}" \
+    "${GLOBAL_MODULE_PWD}/data/${GLOBAL_CURRENT_ENVIRONMENT}/SPA"
+    
+    @comment-strings-where  1 "command:" "${COMPOSE_PATH}"
+    angavel:project:_start "SPA"
+    ws:docker:compose:send-cmd \
+    "${GLOBAL_COMPOSE_ENV_PATH}" "${COMPOSE_PATH}" \
+    "spa_angular" "chmod +x /node-cmd.sh"
+    ws:docker:compose:send-cmd \
+    "${GLOBAL_COMPOSE_ENV_PATH}" "${COMPOSE_PATH}" \
+    "spa_angular" "/node-cmd.sh fresh-install/build"
+    ws:docker:compose:send-cmd \
+    "${GLOBAL_COMPOSE_ENV_PATH}" "${COMPOSE_PATH}" \
+    "spa_webserver" "chmod +x /nginx-cmd.sh"
+    ws:docker:compose:send-cmd \
+    "${GLOBAL_COMPOSE_ENV_PATH}" "${COMPOSE_PATH}" \
+    "spa_webserver" "/nginx-cmd.sh change-group/owner"
+    angavel:project:_stop "SPA"
+    @comment-strings-where  0 "command:" "${COMPOSE_PATH}"
+    
+}
--- lib/logic/project/project.sh
+++ lib/logic/project/project.sh
@@ -17,54 +17,14 @@ function angavel:project:stop {
 
 function angavel:project:start-fresh {
     
-    local LOCAL_API_COMPOSE_PATH="${GLOBAL_COMPOSE_PWD}/API/docker.yml"
-    local LOCAL_SPA_COMPOSE_PATH="${GLOBAL_COMPOSE_PWD}/SPA/docker.yml"
+    
+    
     
     ws:docker:set-network "${GLOBAL_MODULE_NETWORK_NAME}"
     
     angavel:project:stop
     
-    git clone \
-    -b "${ENV_API_GIT_BRANCH}" \
-    "${ENV_API_GIT_URL}" \
-    "${GLOBAL_MODULE_PWD}/data/${GLOBAL_CURRENT_ENVIRONMENT}/API"
-    
-    git clone \
-    -b "${ENV_SPA_GIT_BRANCH}" \
-    "${ENV_SPA_GIT_URL}" \
-    "${GLOBAL_MODULE_PWD}/data/${GLOBAL_CURRENT_ENVIRONMENT}/SPA"
-    
-    angavel:project:_start "API"
-    ws:docker:compose:send-cmd \
-    "${GLOBAL_COMPOSE_ENV_PATH}" "${LOCAL_API_COMPOSE_PATH}" \
-    "api_cgi" "chmod +x /cgi-cmd.sh"
-    ws:docker:compose:send-cmd \
-    "${GLOBAL_COMPOSE_ENV_PATH}" "${LOCAL_API_COMPOSE_PATH}" \
-    "api_cgi" "/cgi-cmd.sh change-group/owner"
-    ws:docker:compose:send-cmd \
-    "${GLOBAL_COMPOSE_ENV_PATH}" "${LOCAL_API_COMPOSE_PATH}" \
-    "api_cgi" "/cgi-cmd.sh composer-install"
-    ws:docker:compose:send-cmd \
-    "${GLOBAL_COMPOSE_ENV_PATH}" "${LOCAL_API_COMPOSE_PATH}" \
-    "api_cgi" "/cgi-cmd.sh artisan-key-gen"
-    angavel:project:_stop "API"
-    
-    @comment-strings-where  1 "command:" "${LOCAL_SPA_COMPOSE_PATH}"
-    angavel:project:_start "SPA"
-    ws:docker:compose:send-cmd \
-    "${GLOBAL_COMPOSE_ENV_PATH}" "${LOCAL_SPA_COMPOSE_PATH}" \
-    "spa_angular" "chmod +x /node-cmd.sh"
-    ws:docker:compose:send-cmd \
-    "${GLOBAL_COMPOSE_ENV_PATH}" "${LOCAL_SPA_COMPOSE_PATH}" \
-    "spa_angular" "/node-cmd.sh fresh-install/build"
-    ws:docker:compose:send-cmd \
-    "${GLOBAL_COMPOSE_ENV_PATH}" "${LOCAL_SPA_COMPOSE_PATH}" \
-    "spa_webserver" "chmod +x /nginx-cmd.sh"
-    ws:docker:compose:send-cmd \
-    "${GLOBAL_COMPOSE_ENV_PATH}" "${LOCAL_SPA_COMPOSE_PATH}" \
-    "spa_webserver" "/nginx-cmd.sh change-group/owner"
-    angavel:project:_stop "SPA"
-    @comment-strings-where  0 "command:" "${LOCAL_SPA_COMPOSE_PATH}"
+    angavel:project:init
     
     angavel:project:start
     
@@ -89,7 +49,7 @@ function angavel:project:start-refresh {
 
 function angavel:project:start {
     
-    # angavel:project:start:srp
+    angavel:project:start:srp
     angavel:project:start:api
     angavel:project:start:spa
     
