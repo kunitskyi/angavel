e76657062c92a9637ed6d072d0da04f7bf59f822
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-05-18 08:37:56 -0400

    add: API config

---
+++ config/API/cgi-cmd.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+FIRST_ARG="$1"
+
+if [ "$FIRST_ARG" = "change-group/owner" ]
+then
+    chgrp www-data /var/www/api
+    chown -R $USER:www-data /var/www/api/storage
+    chmod -R 775 /var/www/api/storage
+elif [ "$FIRST_ARG" = "composer-install" ]
+then
+    composer install
+elif [ "$FIRST_ARG" = "artisan-key-gen" ]
+then
+    php artisan key:generate
+fi
