5f3e96171e7e16e924b7847deb1f7a13b2966caf
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-05-18 08:42:55 -0400

    add: start.sh in project logic

---
+++ lib/logic/project/start.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+##
+#
+# SPA Project Script
+#
+#######
+
+function angavel:project:start:srp {
+    
+    angavel:project:_start "SRP"
+    
+}
+
+function angavel:project:start:api {
+    
+    angavel:project:_start "API"
+    
+}
+
+function angavel:project:start:spa {
+    
+    angavel:project:_start "SPA"
+    
+}
