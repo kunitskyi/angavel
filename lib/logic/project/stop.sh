bedd122e04046b22af563789c54bcfdea64e9b80
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-05-18 08:42:31 -0400

    add: stop.sh

---
+++ lib/logic/project/stop.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+##
+#
+# SPA Project Script
+#
+#######
+
+function angavel:project:stop:srp {
+    
+    angavel:project:_stop "SRP"
+    
+}
+
+function angavel:project:stop:api {
+    
+    angavel:project:_stop "API"
+    
+}
+
+function angavel:project:stop:spa {
+    
+    angavel:project:_stop "SPA"
+    
+}
