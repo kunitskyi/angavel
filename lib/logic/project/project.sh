fcfb39585d5cff3b177147bf41f10b64abbaaa03
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-05-18 14:12:36 -0400

    add: some global variables

--- lib/logic/project/project.sh
+++ lib/logic/project/project.sh
@@ -31,15 +31,13 @@ function angavel:project:start-fresh {
 
 function angavel:project:start-refresh {
     
-    local DATA_DIR_PWD="${GLOBAL_MODULE_PWD}/data/${GLOBAL_CURRENT_ENVIRONMENT}"
-    local CACHE_DIR_PWD="${GLOBAL_MODULE_PWD}/cache/${GLOBAL_CURRENT_ENVIRONMENT}"
     
     echo -e "\033[1;41m!!!REMOVING FOLDERS BELLOW!!!\033[0m"
-    echo -e "\033[1m${DATA_DIR_PWD}\033[0m"
-    echo -e "\033[1m${CACHE_DIR_PWD}\033[0m"
+    echo -e "\033[1m${GLOBAL_DATA_PWD}\033[0m"
+    echo -e "\033[1m${GLOBAL_CACHE_PWD}\033[0m"
     sudo rm -rfI \
-    "${DATA_DIR_PWD}" \
-    "${CACHE_DIR_PWD}"
+    "${GLOBAL_DATA_PWD}" \
+    "${GLOBAL_CACHE_PWD}"
     
     ws:docker:prune
     angavel:project:start-fresh
