25a869f12e5f59a94594f5cae3544b7c58e7d830
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-05-18 10:41:19 -0400

    add: indexing for srp logic

---
+++ lib/logic/srp/.index.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+##
+#
+# MODULE LIBRARY LOGIC SRP Index File
+#
+#######
+
+function MODULE-LIBRARY-LOGIC-SRP-INDEX {
+    
+    local LOCAL_PWD="${GLOBAL_MODULE_PWD}/lib/logic/srp"
+    
+    source $LOCAL_PWD/ssl.sh
+    
+}
+
+MODULE-LIBRARY-LOGIC-SRP-INDEX
