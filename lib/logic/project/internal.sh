c3d8b8371d4ff61c13e0c965275413e83d0c16cd
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-05-18 08:43:30 -0400

    add: internal.sh in project logic

---
+++ lib/logic/project/internal.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+##
+#
+# SPA Project Start
+#
+#######
+
+function angavel:project:_start {
+    
+    # TODO: check $1 in array
+    local COMPOSITION_NAME=$1 # folder ./../../../config/$LOCAL_PROJECT_NAME
+    
+    ws:docker:compose-control \
+    "$GLOBAL_MODULE_PWD/.env/$GLOBAL_CURRENT_ENVIRONMENT/docker.env" \
+    "$GLOBAL_MODULE_PWD/config/$COMPOSITION_NAME/docker.yml" \
+    "up -d"
+    
+}
+
+function angavel:project:_stop {
+    
+    # TODO: check $1 in array
+    local COMPOSITION_NAME=$1 # folder ./../../../config/$LOCAL_PROJECT_NAME
+    
+    ws:docker:compose-control \
+    "$GLOBAL_MODULE_PWD/.env/$GLOBAL_CURRENT_ENVIRONMENT/docker.env" \
+    "$GLOBAL_MODULE_PWD/config/$COMPOSITION_NAME/docker.yml" \
+    "down"
+    
+}
