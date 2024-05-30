5cfd3482bf0ef21d23ab7f2355e40aafbbdbd6dd
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-05-30 09:04:10 -0400

    mv: selector configuration in config folder

--- .lib/.index.sh
+++ .lib/.index.sh
@@ -11,7 +11,7 @@ function MODULE-LIBRARY-INDEX {
     local LOCAL_PWD="${GLOBAL_MODULE_PWD}/.lib"
 
     source $LOCAL_PWD/logic/.index.sh
-    source $LOCAL_PWD/selector/.index.sh
+    source $LOCAL_PWD/config/.selector/.index.sh
 
 }
 
