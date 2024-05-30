4fec35ce8be6f9406618008308e1839a73688a67
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2024-05-30 14:28:48 -0400

    fix: typo (instead of stopping project was starting)

--- .lib/core/project/stop.sh
+++ .lib/core/project/stop.sh
@@ -8,9 +8,9 @@
 
 function angavel:core:project:stop {
 
-    angavel:srp:_start
-    angavel:api:_start
-    angavel:spa:_start
+    angavel:srp:_stop
+    angavel:api:_stop
+    angavel:spa:_stop
 
     angavel:selector:main
 }
